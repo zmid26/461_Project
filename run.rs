7f67ae5ff8826ce21d1079a5341a5f1cdc92cc89
Aidan Caputi <70601851+aidancaputi@users.noreply.github.com>
2023-01-26 20:13:19 -0500

    gitpython cloning. renamed files. improved workflow. (#16)
     repos clone with GitPython. more organized code. better flow

---
+++ run.rs
@@ -0,0 +1,34 @@
+use std::env; //rust stdlib function to get command line args
+//use std::fs; //rust file library
+use std::process::Command; //library to run processes in rust
+
+fn main(){
+
+    //save the command line argument
+    let cli_input: Vec<String> = env::args().collect(); 
+
+    //save off the argument passed to the CLI
+    let arg = &cli_input[1]; 
+
+    if arg.eq("install") == true { //CLI argument was "install"
+
+        //run script to install necessary stuff for rust (rust_installer.py)
+        let _rust_install_command = Command::new("python3").arg("install/rust_installer.py").spawn();
+    
+    }
+    else if arg.eq("build") == true { //CLI argument was "build"
+
+        //build the rammpup calculation code (calculate_RampUp.rs)
+        let _rampup_build_command = Command::new("rustc").arg("local_cloning/calculate_RampUp.rs").arg("--out-dir").arg("local_cloning/").spawn(); //compiles the rust script called calculate_RampUp.rs
+
+    }
+    else if arg.eq("test") == true { //CLI argument was "test"
+        println!("CLI input was: test");
+    }
+    else { //CLI argument was an input file
+
+        //run the rampup calculation (calculate_RampUp)
+        let _run_rampup = Command::new("./local_cloning/calculate_RampUp").arg(&cli_input[1]).spawn(); //runs the rust executable "calculate_RampUp" with the CLI input file
+
+    }
+}
