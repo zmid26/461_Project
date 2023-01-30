e8c7f614458974f791aa7e3f3e412579df958823
Josh Christian <joshchristian280@gmail.com>
2023-01-29 20:06:57 -0500

    initial GraphQL API usage w/o token

--- graphql_api/graphqltest.py
+++ graphql_api/graphqltest.py
@@ -1,7 +1,6 @@
 import requests
 
-api_token = "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"
-#api_token = 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
+api_token = "insert_token"
 
 url = 'https://api.github.com/graphql'
 json = { 'query' : '{ viewer { repositories(first: 30) { totalCount pageInfo { hasNextPage endCursor } edges { node { name } } } } }' }
