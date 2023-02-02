5c61ed4583fcdd0eb32ef0014ff6a2b80c3451e1
aidancaputi <aidancaputi@gmail.com>
2023-02-01 19:39:28 -0500

    clones all URLs from input file, git and npm

--- local_cloning/calculate_RampUp.rs
+++ local_cloning/calculate_RampUp.rs
@@ -15,8 +15,26 @@ fn main(){
 
     //now, chop this string into a vector at every newline since the URLS are newline delimited
     let _urls: Vec<&str> = data.split('\n').collect();
+
+    //get number of URLS
+    let num_urls = _urls.len();
+    println!("number of urls passed in: {}", num_urls);
+
+    //loop through urls and print their corresponding clone folder numbers
+    let mut url_index = 1;
+    for url in _urls {
+        println!("folder number / url     :     {} / {}", url_index, url);
+        url_index += 1;
+    }
     
-    //runs python script to clone all the repos into directories labeled by number (first is '1')
-    let _run_clone_script = Command::new("python3").arg("local_cloning/clone_repo.py").arg(&cli_input[1]).spawn();
+    //run clone function to locally clone repos
+    clone_repos((&filepath).to_string());
+}
+
+//this function simply runs the file "clone_repo.py" which is located instide of the "461_project/local_cloning" directory
+fn clone_repos(filepath: String){
+
+    //run clone_repo.py
+    let _run_clone_script = Command::new("python3").arg("local_cloning/clone_repo.py").arg(&filepath).output();
 }
 
--- rest_api/test.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-
-    let temp:i32 = 809243;
-
-
-    println!("hello world: {}",temp)
-}
