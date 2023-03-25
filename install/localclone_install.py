b4d19048a7e4b1e08baa5d90f60c12326b3229ef
Elijah Klein <70978037+ElijahKlein@users.noreply.github.com>
2023-03-25 18:48:19 -0400

    Update localclone_install.py
    Adding install for requests module

--- install/localclone_install.py
+++ install/localclone_install.py
@@ -7,6 +7,9 @@ sys.stderr = devnull
 if not os.system("pip3 install GitPython"): #install gitpython
     os.system("pip3 install GitPython") #install gitpython with pip3
 
+if not os.system("pip3 install requests"): #install requests
+    os.system("pip3 install requests") #install requests with pip3
+
 os.system("cargo update -p ignore --precise 0.4.18")
 
 exit(0) #exit 0 on success
