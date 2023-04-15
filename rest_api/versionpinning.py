15d071d42005d5b234dff6cd8709d905ef1bd565
Jack Kwan <kwan6@purdue.edu>
2023-04-15 19:23:20 -0400

    Move file and change base functionality

---
+++ rest_api/versionpinning.py
@@ -0,0 +1,78 @@
+import base64
+import json
+import os
+import re
+import urllib.request as url
+import sys
+import requests
+
+class BadRequest(Exception):
+    '''Define bad request exception'''
+    pass
+
+def getGithubURLs(repo):
+    '''Transform npm links to github links'''
+    webUrl = url.urlopen(repo)
+    if webUrl.getcode() == 200:
+        html_cont = webUrl.read().decode("utf-8")
+        r1 = r'<span id="repository-link">(.*?)<\/span>'
+        try:
+            reg_out = re.search(r1, html_cont)
+            gitLink = "https://" + reg_out.group(1)
+        except:
+            raise Exception("Valid GitHub link not found.\n")
+    else:
+        raise Exception("npm url not able to connect.\n")
+    return gitLink
+
+def count_pinned_dependencies(repo_url):
+    '''Counts the pinned dependencies'''
+    # Parse the repository owner and name from the URL
+    match = re.match(r'https://github.com/([\w-]+)/([\w-]+)', repo_url)
+    if not match:
+        raise BadRequest('Invalid repository URL')
+    owner = match.group(1)
+    repo_name = match.group(2)
+
+    # Fetch the repository metadata from the GitHub API
+    token = os.environ.get('GITHUB_TOKEN')
+    headers = {'Authorization': f'token {token}'}
+    api_url = f'https://api.github.com/repos/{owner}/{repo_name}/contents/package.json'
+    response = requests.get(api_url, headers=headers)
+    if response.status_code != 200:
+        if response.status_code == 401:
+            raise BadRequest('Failed to authenticate with GitHub API. Please check your personal access token.')
+        elif response.status_code == 403:
+            raise BadRequest('Failed to access repository. Please ensure your personal access token has the necessary permissions.')
+        else:
+            raise BadRequest(f'Failed to fetch package.json. Error {response.status_code}: {response.reason}')
+
+    # Parse the contents of package.json and count the number of pinned dependencies
+    contents = json.loads(base64.b64decode(response.json()['content']).decode('utf-8'))
+    count = 0
+    pinned = 0
+    if 'dependencies' in contents:
+        for dep, version in contents['dependencies'].items():
+            print(dep, version)
+            count += 1
+            if re.match(r'^\^\d+\.\d+\.+', version) or re.match(r'^\d+\.\d+\.+', version):
+                pinned += 1
+    else:
+        print("No dependencies found, returning 1.0")
+        return 1.0
+    if pinned == 0:
+        return 1.0
+    else:
+        return pinned / count
+def main():
+    '''Main function'''
+    urlInput = sys.argv[1]
+    if "npmjs.com/" in urlInput:
+        gitURL = getGithubURLs(urlInput)
+    else:
+        gitURL = urlInput
+    count_dep = count_pinned_dependencies(gitURL)
+    print(count_dep)
+if __name__ == "__main__":
+    main()
+    
