1aa01eb53b1299691cd66be8a83c5a2bc08d564d
Matthew-Nale <71462426+Matthew-Nale@users.noreply.github.com>
2023-04-17 00:48:03 -0400

    Bad linter changes

--- test_suite/test_suite_script.py
+++ test_suite/test_suite_script.py
@@ -4,7 +4,7 @@ from subprocess import DEVNULL
 net_scores = []
 # open files containing urls and extract urls
 with open("test_suite/good_urls.txt") as good_urls:
-    urls = good_urls.readlines() 
+    urls = good_urls.readlines()
 with open("test_suite/bad_urls.txt") as bad_urls:
     urls.extend(bad_urls.readlines())
 
@@ -14,7 +14,11 @@ for url in urls:
         newTestFile.write(url.strip())
 
     # run each individual script for metric calculation
-    printed_result = str(subprocess.run(["./run", "score", "test_suite/newTest.txt"], stdout=subprocess.PIPE).stdout)
+    printed_result = str(
+        subprocess.run(
+            ["./run", "score", "test_suite/newTest.txt"], stdout=subprocess.PIPE
+        ).stdout
+    )
 
     # extract net_score value from output
     try:
@@ -23,12 +27,17 @@ for url in urls:
     except:
         net_score = -1
 
-    if net_score <= 1 and net_score >= 0:  # if the net_score is valid, add it as a passed test
+    if (
+        net_score <= 1 and net_score >= 0
+    ):  # if the net_score is valid, add it as a passed test
         net_scores.append(net_score)
 
 print("Total: " + str(len(urls)))
 print("Passed: " + str(len(net_scores)))
 print("Coverage: n/a%")
-print("%i/%i test cases passed. n/a%% line coverage achieved" % (len(net_scores), len(urls)))
+print(
+    "%i/%i test cases passed. n/a%% line coverage achieved"
+    % (len(net_scores), len(urls))
+)
 
 exit(0)
