3069f7ce363af7cc3d496a5c39a09e7c61ec0bb7
Matthew-Nale <71462426+Matthew-Nale@users.noreply.github.com>
2023-04-17 00:40:53 -0400

    Bad linter changes

--- rest_api/calculate_ResponsiveMaintainer.py
+++ rest_api/calculate_ResponsiveMaintainer.py
@@ -20,27 +20,34 @@ def getResponsiveScore(githubRepoURL):
 
     github_token = os.getenv("GITHUB_TOKEN")
 
-
     file_v2 = open("log/logv1.txt", "a+")
     file_v3 = open("log/logv2.txt", "a+")
 
     file_v2.write("\n\n>>> beginning respmaintainer metric with REST api\n")
 
     file_v3.write("\n\n------------------\n")
-    file_v3.write("current analysis of responsive maintainer metric will be done with github REST api\n")
-    file_v3.write("beginning retrieval of information from repository %s\n" % githubRepoURL)
+    file_v3.write(
+        "current analysis of responsive maintainer metric will be done with github REST api\n"
+    )
+    file_v3.write(
+        "beginning retrieval of information from repository %s\n" % githubRepoURL
+    )
     file_v3.write("------------------\n")
 
 
-    headers = {"Authorization": f"token {github_token}"}  # build the header for authentication
+    headers = {
+        "Authorization": f"token {github_token}"
+    }  # build the header for authentication
    
     # get a response using the REST API
-    openResp = (rq.get(url=openURL, headers=headers))
+    openResp = rq.get(url=openURL, headers=headers)
     # if the response is successful, get the issue numbers
     if openResp.status_code == 200:
-        file_v2.write('able to receive response from github\n')
-        file_v3.write('able to receive response code %d from github\n' % openResp.status_code)
-        
+        file_v2.write("able to receive response from github\n")
+        file_v3.write(
+            "able to receive response code %d from github\n" % openResp.status_code
+        )
+
         # test if the repos contain the correct data in json format
         try:
             respJson = openResp.json()
@@ -53,14 +60,18 @@ def getResponsiveScore(githubRepoURL):
             year = int(updatedDate[0])
             month = int(updatedDate[1])
             day = int(updatedDate[2].split("T")[0])
-            updatedDate = dt.date(year,month,day)
+            updatedDate = dt.date(year, month, day)
 
             file_v2.write("proper repo format - data retrieval successful\n")
-            file_v3.write("proper repo format - retrieved %s from the api\n" % updatedDate)
+            file_v3.write(
+                "proper repo format - retrieved %s from the api\n" % updatedDate
+            )
 
         except:
             file_v2.write("improper repo format")
-            file_v3.write("improper repo format- investigate repo at %s \n" % githubRepoURL)
+            file_v3.write(
+                "improper repo format- investigate repo at %s \n" % githubRepoURL
+            )
 
             return -1
 
@@ -76,23 +87,30 @@ def getResponsiveScore(githubRepoURL):
         if openNum > 25:
             file_v2.write("number of open issues exceeded threshold\n")
             file_v3.write("number of open issues exceeded threshold of 25\n")
-            score += min(0.2,0.2 * ((MAXNUMOPEN - openNum) / MAXNUMOPEN))
+            score += min(0.2, 0.2 * ((MAXNUMOPEN - openNum) / MAXNUMOPEN))
 
         elapsedTime = str(dt.date.today() - updatedDate)
         if elapsedTime == "0:00:00":
             file_v2.write("repo was updated today\n")
-            file_v3.write("repo was updated today, i.e. day difference was %s\n" % elapsedTime)
+            file_v3.write(
+                "repo was updated today, i.e. day difference was %s\n" % elapsedTime
+            )
             elapsedTime = 0
         else:
             elapsedTime = int(elapsedTime.split(" ")[0])
             file_v2.write("repo was not updated today\n")
-            file_v3.write("repo was not updated today, it was updated %d days ago\n" % elapsedTime)
+            file_v3.write(
+                "repo was not updated today, it was updated %d days ago\n" % elapsedTime
+            )
             if elapsedTime < 0:
                 elapsedTime = 0
 
         score += 0.75 * (UPDATEDECAY ** (-1 * elapsedTime))
 
-        file_v3.write("responsive maintainer score was calculated to be %f with decay factor %f\n" % (score,UPDATEDECAY))
+        file_v3.write(
+            "responsive maintainer score was calculated to be %f with decay factor %f\n"
+            % (score, UPDATEDECAY)
+        )
 
         file_v2.close()
         file_v3.close()
@@ -104,7 +122,10 @@ def getResponsiveScore(githubRepoURL):
         # print('failed to resolve repository: ',githubRepoURL,' as ',openURL)
         # print('openResp.status_code: ',openResp.status_code)
         file_v2.write("failed to resolve repository\n")
-        file_v3.write("failed to resolve repository with response code %d from github\n" % openResp.status_code)
+        file_v3.write(
+            "failed to resolve repository with response code %d from github\n"
+            % openResp.status_code
+        )
 
         file_v2.close()
         file_v3.close()
@@ -125,9 +146,13 @@ def getGithubURLs(repos):
 
             if not isinstance(repoName, str):  # if a dict is returned instead of str
                 repoName = list(repoName.values())[1]  # extract url from dict
-                repoName = repoName.partition("github.com/")[2].replace(".git", "")  # extract "owner/repo"
-    
-        repositories.append((repoName.partition("/")[0],repoName.partition("/")[2].replace("\n","")))  # append (owner, repo)
+                repoName = repoName.partition("github.com/")[2].replace(
+                    ".git", ""
+                )  # extract "owner/repo"
+
+        repositories.append(
+            (repoName.partition("/")[0],repoName.partition("/")[2].replace("\n",""))
+        )  # append (owner, repo)
 
     # convert all repos to github urls
     gitLinks = []
@@ -138,6 +163,7 @@ def getGithubURLs(repos):
 
     # return list of github urls
     return gitLinks
+
 
 def main():
 
@@ -159,5 +185,7 @@ def main():
             # print('\nResponsive Maintainer score for repo: ', u, '\nis: ',currScore,'\n')
             f.write(str(currScore))
             f.write("\n")
+
+
 if __name__ == "__main__":
     main()
