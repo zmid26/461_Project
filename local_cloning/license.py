470ee691c008edfb61f50a9e7104d8dae485e4dc
Matthew-Nale <71462426+Matthew-Nale@users.noreply.github.com>
2023-04-17 00:32:10 -0400

    Linter fixes

--- local_cloning/license.py
+++ local_cloning/license.py
@@ -16,10 +16,10 @@ with open("output/license_out.txt", "w") as f:
             for root, dirs, files in os.walk(d):
                 for x in files:
                     if readme_pattern.match(x) and found == 0:
-                        valid_readme_file = os.path.join(root,x)
+                        valid_readme_file = os.path.join(root, x)
                         with open(valid_readme_file, "r") as readme:
                             text = readme.read()
-                            if("license" or "LICENSE" or "License" in text):
+                            if "license" or "LICENSE" or "License" in text:
                                 f.write(str(1.0))
                                 f.write("\n")
                             else:
