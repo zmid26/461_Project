1adedbf34b98407c74bb842e8543f7b04087ba9e
Matthew-Nale <71462426+Matthew-Nale@users.noreply.github.com>
2023-04-16 23:57:05 -0400

    Linter fixes

--- verbosity.py
+++ verbosity.py
@@ -1,35 +1,33 @@
 import os
 import sys
 
-devnull = open('/dev/null', 'w')
+devnull = open("/dev/null", "w")
 sys.stdout = devnull
 sys.stderr = devnull
 
-env_file = open('.env')
+env_file = open(".env")
 vars = env_file.readlines()
 
-
-log_fstr = os.getenv('LOG_FILE')
+log_fstr = os.getenv("LOG_FILE")
 log_fstr = str(log_fstr)
 
-logLvl = os.getenv('LOG_LEVEL')
+logLvl = os.getenv("LOG_LEVEL")
 logLvl = int(logLvl)
 
 
-
 if logLvl == 1:
-    logTemp = open('log/logv1.txt','r')
+    logTemp = open("log/logv1.txt", "r")
     logInfo = logTemp.readlines()
 elif logLvl == 2:
-    logTemp = open('log/logv2.txt','r')
+    logTemp = open("log/logv2.txt", "r")
     logInfo = logTemp.readlines()
 else:
     logInfo = []
-  
+
 try:
-    log_true = open(log_fstr,'w+')
+    log_true = open(log_fstr, "w+")
 except:
-    log_true = open('log_file.txt','w+')
+    log_true = open("log_file.txt", "w+")
 
 
 for t in logInfo:
