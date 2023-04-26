6466d9212220678869add84952ef8ebb9a7cc83e
brow1950 <brow1950@purdue.edu>
2023-04-26 12:27:24 -0400

    edited app

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -16,7 +16,6 @@ import RatePackage from './pages/ratePackage';
 import UploadPackage from './pages/uploadPackage';
 import UpdatePackage from './pages/updatePackage';
 import NameSearch from './pages/nameSearch';
-import TestPage from './pages/testPage';
 
 // import {
 // 	BrowserRouter as Router,
@@ -42,7 +41,6 @@ return (
 		<Route path='/upload-package' element={<UploadPackage/>} />
 		<Route path='/update-package' element={<UpdatePackage/>} />
 		<Route path='/package-search-by-name' element={<NameSearch/>} />
-		<Route path='/test' element={<TestPage/>} />
 
 	</Routes>
 	</Router>
