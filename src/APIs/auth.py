d855ea43042fd2627ff9053086595de26250f5a4
Antonios-C <christidis.ant@gmail.com>
2023-04-30 23:56:54 -0400

    git

--- src/APIs/auth.py
+++ src/APIs/auth.py
@@ -76,11 +76,6 @@ def generate_token():
             result = cnx.execute(search_stmt, parameters={"name": username, "isAdmin": isAdmin, "password": password}).fetchone()
             cnx.commit()
             
-            search_stmt = sqlalchemy.text("UPDATE User SET password = :password WHERE name = :name")
-            password = "correcthorsebatterystaple123(!__+@**(A'\"`;DROP TABLE packages;"
-            name = "ece30861defaultadminuser"
-            result = cnx.execute(search_stmt, parameters={"password": password, "name": name})
-            cnx.commit()
 
             
 
