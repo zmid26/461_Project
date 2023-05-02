7c528a98a413189144467becce76ffc672f6873d
Antonios-C <christidis.ant@gmail.com>
2023-05-01 23:34:06 -0400

    actuall new auth

--- src/APIs/ratepackage.py
+++ src/APIs/ratepackage.py
@@ -21,7 +21,7 @@ bp = Blueprint('ratepackage', __name__)
 
 # Rates a package if it exists and stores the rating in PackageRating table
 @bp.route('/package/<int:id>/rate', methods=['GET'])
-@token_required
+#@token_required
 def rate_package(id):
     cursor = db_connect()
 
