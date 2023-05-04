17120695f536fadf639fe2397427030ff0b81971
Antonios-C <christidis.ant@gmail.com>
2023-05-03 22:28:43 -0400

    ff

--- src/APIs/packageid.py
+++ src/APIs/packageid.py
@@ -56,21 +56,23 @@ def package():
     cnx = db_connect()
     global idvalue
     idvalue += 1
-    # Expect JSON input
+    # Expect JSON input and validate that there aren't any missing parts 
     if request.is_json:
         try:
             validate(request.json, input_schema)
             print(f"PATH (post package): {request.path} {request.method}")
             print(f"REQUEST BODY: {str(request.get_data())}")
+
+            #validate that there exists at least a 1 of the two (content or ULR) which isn't null
             if request.json["Content"] == None and request.json["URL"] == None:
               print(f"post package both content and url are null")
               return make_response('', 400)
             
+            #decode the content and extra the url , this code executes when URL is none
             if "Content" in request.json and request.json["Content"] != None:
             #if "Content" in request.json:
               content = request.json["Content"]
               jsprog = request.json["JSProgram"]
-            
             
               zip_file_bytes = base64.b64decode(content)
               zip_file_io = io.BytesIO(zip_file_bytes)
@@ -87,7 +89,7 @@ def package():
                     url = data['repository']['url']
                   else:
                     zero = 0
-
+            #use the github api to get encoded content or other metadata, this code executes when Content is none
             if "URL" in request.json and request.json["URL"] != None:
               jsprog = request.json["JSProgram"]
               url = request.json["URL"]
@@ -106,25 +108,15 @@ def package():
             search_stmt = sqlalchemy.text("SELECT * FROM Package WHERE Name=:name AND Version = :version")
             package = cnx.execute(search_stmt, parameters={"name": name, "version": version}).fetchone()
             cnx.commit()
-
+            #check if package already exists
             if package:
               return make_response('', 409)
             
-            rate_url = "https://project461-web-fgnl7r7dtq-uc.a.run.app/package/" + str(id) + "/rate"
-            rResponse = requests.get(rate_url)
-            data = json.loads(rResponse.content)
-            if rResponse.status_code == 200:
-              print(f"RATE API RETURNED 200")
-              for key, value in data.items():
-                if isinstance(value, int) and value < 0.5:
-                  print(f"DISQUALIED")
-                  return make_response('', 424)
-            
-
+            #insert data into database
             insert_stmt = sqlalchemy.text("INSERT INTO Package (ID, Name, Version, Content, JSProgram, URL) VALUES (:idvalue, :name, :version, :content, :jsprog, :url)")
             cnx.execute(insert_stmt, parameters={"idvalue": idvalue, "name": name, "version": version, "content": content, "jsprog": jsprog, "url": url})
             cnx.commit()
-
+            #format response and return 
             metadata = {
             "Name": name,
             "Version": version,
@@ -137,14 +129,13 @@ def package():
             response = {"metadata": metadata, "data": data}
             print(f"RESPONSE BODY: {response}")
             return jsonify(response), 201 
-            
         except jsonschema.exceptions.ValidationError as err:
             print("schema error for post package")
             return make_response('', 400)
     else:
         return make_response('', 424)
 
-# Function to interact with database
+# Function to interact with database - legacy code 
 def post_package(content, jsprog, cur, name, version, url):
     insert_query = "INSERT INTO Package (ID, Name, Version, Content, JSProgram, URL) VALUES (%s, %s, %s, %s, %s, %s)"
     values = (idvalue, name, version, content, jsprog, url)
@@ -157,17 +148,17 @@ def get_package(id):
   print(f"PATH (get): {request.path} {request.method}")
   print(f"trying to get id = {id}")
   print(f"REQUEST BODY of get: {str(request.get_data())}")
-  
+  #connect and search if package exists in database
   cnx = db_connect()
-  
   search_stmt = sqlalchemy.text("SELECT * FROM Package WHERE ID=:id")
   package = cnx.execute(search_stmt, parameters={"id": id}).fetchone()
   cnx.commit()
 
+  #check if packages exists
   if package is None:
     print(f"package wanted to get doesn't exist")
     return make_response('', 404)
-  
+  #format get response
   metadata = {
     "Name": package[1],
     "Version": package[2],
@@ -271,11 +262,11 @@ def put_package(id):
       content = metadata["Content"]
       url = metadata["URL"]
       jsprogram = metadata["JSProgram"]
-
+      #check if updated data has either url or content as thats required to continue
       if url == None and content == None:
         print(f"both url and content is null for update package {id}")
         return make_response('', 400)
-
+      #if url is null then use content to get the url 
       if url == None:
         zip_file_bytes = base64.b64decode(content)
         zip_file_io = io.BytesIO(zip_file_bytes)
@@ -290,12 +281,13 @@ def put_package(id):
               url = data['homepage']
             else:
               zero = 0
-
+      #if content is null then use url and the github api to get the content 
       if content == None:
         owner, repo = url.split('/')[-2:]
         response = requests.get(f"https://api.github.com/repos/{owner}/{repo}/zipball")
         content = base64.b64encode(response.content).decode('utf-8')
-      #update
+      
+      #update the database
       update_package(id, cnx, name, version, content, url, jsprogram)
 
       return make_response('', 200)
@@ -322,11 +314,12 @@ def delete_package(id):
 
   # Get package from the database
   package = get_package(id, cnx)
-
+  #check if packages doesn't exist
   if package is None:
     return make_response('', 404)
-  
+  #delete the packages 
   delete_from_db(id, cnx)
+
   return make_response('', 200)
 
 # Function to interact with database
@@ -334,14 +327,7 @@ def delete_from_db(id, cnx):
     search_stmt = sqlalchemy.text("DELETE FROM Package WHERE ID =:id")
     cnx.execute(search_stmt, parameters={"id": id})
     cnx.commit()
-    '''
-    cnx.reconnect()
-    cur = cnx.cursor()
-    cur.execute("DELETE FROM Package WHERE ID = %s", (id,))
-    cnx.commit()
-    cur.close()
-    cnx.close()
-    '''
+    
 
 
 @bp.route('/reset', methods=['DELETE'], endpoint = 'resetEND')
@@ -350,7 +336,7 @@ def reset_package():
   print(f"PATH (/reset): {request.path} {request.method}")
   print(f"REQUEST BODY: {str(request.get_data())}")
   cnx = db_connect()
-
+  #Clear tables
   search_stmt = sqlalchemy.text("DELETE FROM Package")
   cnx.execute(search_stmt)
   cnx.commit()
@@ -371,11 +357,10 @@ input_schema4 = {
 @bp.route('/package/byRegEx', methods=['POST'], endpoint = 'regExEND')
 @token_required
 def regex_package():
-  #return make_response('', 502)
   print(f"PATH (POST RegEx): {request.path} {request.method}")
   print(f"REQUEST BODY: {str(request.get_data())}")
   cnx = db_connect()
-
+  #check if all json field are present
   if request.is_json:
     try:
       validate(request.json, input_schema4)
@@ -385,6 +370,7 @@ def regex_package():
       results = cnx.execute(search_stmt, parameters={":reg": reg}).fetchall()
       cnx.commit()
 
+      #format response
       packages = []
       for row in results:
         package = {'Version': row[0], 'Name': row[1]}
--- src/api_calls/auth_test.sh
+++
@@ -1,9 +0,0 @@
-curl -H "Content-Type: application/json" -d '{
-  "User": {
-    "name": "root",
-    "isAdmin": true
-  },
-  "Secret": {
-    "password": "password1234"
-  }
-}' -X PUT localhost:5000/authenticate -v
